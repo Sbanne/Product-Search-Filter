d29ed83087307ee369763e83694b4ffbad6bb6a9
Sujit Banne <sujitbanne13@gmail.com>
2022-12-19 14:20:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const search = () => {
+    const searchBox = document.getElementById("search-item").value.toUpperCase();
+    const storeItems = document.getElementById("product-list");
+    const product = document.querySelectorAll(".product")
+    const pName = storeItems.getElementsByTagName("h2")
+
+    for (let i = 0; i < pName.length; i++) {
+        let match = product[i].getElementsByTagName("h2")[0]
+
+        if (match) {
+            let textValue = match.textContent || match.innerHTML
+            console.log(match.textContent);
+
+            if (textValue.toUpperCase().indexOf(searchBox) > -1) {
+                product[i].style.display = "";
+            }
+            else {
+                product[i].style.display = "none";
+            }
+        }
+    }
+}
